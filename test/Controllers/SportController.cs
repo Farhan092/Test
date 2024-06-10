cf376cbfeef03ba643ee9f7d2f8106d5b3bdf116
Farhan Amin <133957646+Farhan092@users.noreply.github.com>
2024-06-10 11:51:31 +0600

    Add files via upload

---
+++ test/Controllers/SportController.cs
@@ -0,0 +1,67 @@
+﻿using labtask.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace labtask.Controllers
+{
+    public class SportController : Controller
+    {
+        // GET: Sport
+     
+
+        public ActionResult Player()
+        {
+
+            List<Player> list = new List<Player>() {
+            new Player { Id = 1 ,Name = "farhan",Salary = "10000"},
+            new Player { Id = 2, Name = "siam", Salary = "10000" },
+            new Player { Id = 3, Name = "shahriar", Salary = "10000" }
+
+            };
+
+            ViewBag.List = list;
+          
+
+            return View();
+        }
+
+        public ActionResult Staff()
+        {
+
+            List<Staff> list = new List<Staff>() {
+            new Staff { Id = 1 ,Name = "farhan",Position = "salesman"},
+            new Staff { Id = 2, Name = "siam", Position = "employee" },
+            new Staff { Id = 3, Name = "shahriar", Position = "manager" }
+
+            };
+
+            ViewBag.List = list;
+
+
+            return View();
+        }
+
+        public ActionResult Stadium()
+        {
+
+            List<Stadium> list = new List<Stadium>() {
+            new Stadium { Location = "sylhet" ,Capacity = "Fifty Thousand"},
+            new Stadium { Location = "Dhaka", Capacity = "Sixty Thousand" },
+     
+
+            };
+
+            ViewBag.List = list;
+
+
+            return View();
+        }
+
+
+
+
+    }
+}
---
+++ test/Models/Player.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace labtask.Models
+{
+    public class Player
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Salary { get; set; }
+
+      
+    }
+
+    public class Staff
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Position { get; set; }
+    }
+
+    public class Stadium
+    {
+        
+        public string Location { get; set; }
+        public string Capacity { get; set; }
+    }
+}
