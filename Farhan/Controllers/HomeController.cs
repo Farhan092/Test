d4a6c6f89cbc9dff0cfe962d2f39bbddc584066e
Farhan Amin <133957646+Farhan092@users.noreply.github.com>
2024-06-24 00:12:10 +0600

    Add files via upload

---
+++ Farhan/Controllers/HomeController.cs
@@ -0,0 +1,62 @@
+﻿using practice.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace practice.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Test()
+        {
+            //return View();
+            return Content("hi there");
+        }
+
+        public ActionResult Contact(int id)
+        {
+            //return View();
+
+            string result = "siam sarker" + id;
+            return Content(result);
+        }
+
+        public string ABC(int x)
+        {
+             return "siam sarker" + x;
+
+            
+
+
+
+        }
+
+        public ActionResult XYZ()
+        {
+            return View("ABC");
+        }
+
+
+        public ActionResult Club()
+        {
+
+
+            return Content("hi there");
+        }
+
+
+
+
+
+
+
+    }
+}
---
+++ Farhan/Controllers/SportController.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using practice.Models;
+using practice.ViewModel;
+
+namespace practice.Controllers
+{
+    public class SportController : Controller
+    {
+
+
+        public ActionResult Player()
+        {
+
+            List<Player> list = new List<Player>() {
+            new Player { Id = 1 ,Name = "farhan",Salary = "10000"},
+            new Player { Id = 2, Name = "siam", Salary = "10000" },
+            new Player { Id = 3, Name = "shahriar", Salary = "10000" }
+
+            };
+
+            ViewBag.List = list;
+
+
+            return View();
+        }
+
+        public ActionResult Staff()
+        {
+            List<Staff> list = new List<Staff>() {
+            new Staff { Id = 1 ,Name = "farhan",Position = "salesman"},
+            new Staff { Id = 2, Name = "siam", Position = "employee" },
+            new Staff { Id = 3, Name = "shahriar", Position = "manager" }
+
+            };
+
+            ViewBag.List = list;
+
+
+            return View();
+        }
+
+
+        [Route("Sport/stadium")]
+        public ActionResult Stadium()
+        {
+
+            List<Stadium> list = new List<Stadium>() {
+            new Stadium { Location = "sylhet" ,Capacity = "Fifty Thousand"},
+            new Stadium { Location = "Dhaka", Capacity = "Sixty Thousand" },
+
+
+            };
+
+            /*ViewBag.List = list;*/
+
+
+            return View(list);
+        }
+
+        [Route("combined/abc")]
+        public ActionResult SampleModel()
+        {
+            List<Player> players = new List<Player>() {
+        new Player { Id = 1 ,Name = "farhan", Salary = "10000" },
+        new Player { Id = 2, Name = "siam", Salary = "10000" },
+        new Player { Id = 3, Name = "shahriar", Salary = "10000" }
+    };
+
+            List<Staff> staffs = new List<Staff>() {
+        new Staff { Id = 1 ,Name = "farhan", Position = "salesman" },
+        new Staff { Id = 2, Name = "siam", Position = "employee" },
+        new Staff { Id = 3, Name = "shahriar", Position = "manager" }
+    };
+
+            List<Stadium> stadiums = new List<Stadium>() {
+        new Stadium { Location = "sylhet", Capacity = "Fifty Thousand" },
+        new Stadium { Location = "Dhaka", Capacity = "Sixty Thousand" }
+    };
+
+            var viewModel = new SampleModel
+            {
+                Player = players,
+                Staff = staffs,
+                Stadium = stadiums
+                
+            };
+
+            return View(viewModel);
+        }
+
+
+
+        public ActionResult OOP()
+        {
+
+            return View(Club());
+         } 
+
+
+    }
+
+
+
+}
