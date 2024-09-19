dce9d5125db3b8829987affcb8cb21083c061c89
Farhan Amin <133957646+Farhan092@users.noreply.github.com>
2024-09-19 16:44:36 +0600

    Add files via upload

---
+++ New folder (2)/Controllers/GamesController.cs
@@ -0,0 +1,234 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication1.Context;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers.api
+{
+    public class GamesController : Controller
+    {
+        private AppDBGame db = new AppDBGame();
+
+        // GET: Games
+        public ActionResult Index()
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                client.BaseAddress = new Uri(@"http://localhost:55347/api/games"); // http://..api/game
+                var response = client.GetAsync("games"); // route name inside it; last address: games (example)
+                response.Wait();
+
+                if (response.Result.IsSuccessStatusCode)
+                {
+                    var data = response.Result.Content.ReadAsAsync<IEnumerable<Game>>().Result;
+                    return View(data);
+
+                }
+                else
+                    return HttpNotFound();
+            }
+
+            //return View(db.Game.ToList());
+        }
+
+        // GET: Games/Details/5
+        public ActionResult Details(int? id)
+        {
+            /*if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Game game = db.Game.Find(id);
+            if (game == null)
+            {
+                return HttpNotFound();
+            }
+            return View(game);*/
+
+            using (HttpClient client = new HttpClient())
+            {
+                client.BaseAddress = new Uri(@"http://..api/games");
+                var response = client.GetAsync("games/" + id.ToString());
+
+
+                if (response.Result.IsSuccessStatusCode)
+                {
+                    var data = response.Result.Content.ReadAsAsync<IEnumerable<Game>>().Result;
+                    return View(data);
+
+                }
+                return HttpNotFound();
+            }
+
+        }
+
+        // GET: Games/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+
+        // POST: Games/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+
+        public ActionResult Create(Game game)
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                client.BaseAddress = new Uri(@"http://..api/addgames");
+                var response = client.PostAsJsonAsync("addgames", game);
+                response.Wait();
+
+                if (response.Result.IsSuccessStatusCode)
+                {
+                    return RedirectToAction("index");
+
+
+                }
+                else
+                {
+                    return HttpNotFound();
+                }
+
+            }
+        }
+
+
+
+
+
+
+        /*public ActionResult Create([Bind(Include = "Id,Name,Description,Price")] Game game)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Game.Add(game);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(game);
+        }*/
+
+        // GET: Games/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Game game = db.Game.Find(id);
+            if (game == null)
+            {
+                return HttpNotFound();
+            }
+            return View(game);
+        }
+
+        // POST: Games/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(Game game)
+        {
+
+            using (HttpClient client = new HttpClient())
+            {
+                client.BaseAddress = new Uri(@"http://..api/updategames");
+                var response = client.PutAsJsonAsync("updategames/" + game.Id.ToString(), game);
+                response.Wait();
+
+                if (response.Result.IsSuccessStatusCode)
+                {
+                    return RedirectToAction("index");
+                }
+                else
+                {
+                    return HttpNotFound();
+                }
+
+            }
+
+
+
+
+
+
+
+
+            /* if (ModelState.IsValid)
+             {
+                 db.Entry(game).State = EntityState.Modified;
+                 db.SaveChanges();
+                 return RedirectToAction("Index");
+             }
+             return View(game);*/
+        }
+
+        // GET: Games/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Game game = db.Game.Find(id);
+            if (game == null)
+            {
+                return HttpNotFound();
+            }
+            return View(game);
+        }
+
+        // POST: Games/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            /*Game game = db.Game.Find(id);
+            db.Game.Remove(game);
+            db.SaveChanges();
+            return RedirectToAction("Index");*/
+
+
+
+            using (HttpClient client = new HttpClient())
+            {
+                client.BaseAddress = new Uri(@"http://..api/deletegames");
+                var response = client.DeleteAsync("deletegames/" + id.ToString());
+                response.Wait();
+
+                if (response.Result.IsSuccessStatusCode)
+                {
+                    return RedirectToAction("index");
+                }
+                else
+                {
+                    return HttpNotFound();
+                }
+
+            }
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ New folder (2)/Controllers/api/GameController.cs
@@ -0,0 +1,121 @@
+﻿using System.Collections.Generic;
+using System.Data.Entity.Infrastructure;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+using WebApplication1.Context;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers.api
+{
+    //[RoutePrefix("api/games")]
+    public class GamesApiController : ApiController
+    {
+        private readonly AppDBGame db = new AppDBGame();
+
+        // GET: api/games
+        [HttpGet]
+        [Route("api/games")]
+        public async Task<IHttpActionResult> GetGames()
+        {
+            var games = await db.Game.ToListAsync();
+            return Ok(games);
+        }
+
+        // GET: api/games/5
+        [HttpGet]
+        [Route("{id:int}")]
+        public async Task<IHttpActionResult> GetGame(int id)
+        {
+            var game = await db.Game.FindAsync(id);
+            if (game == null)
+            {
+                return NotFound();
+            }
+            return Ok(game);
+        }
+
+        // POST: api/games
+        [HttpPost]
+        [Route("")]
+        public async Task<IHttpActionResult> CreateGame([FromBody] Game game)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.Game.Add(game);
+            await db.SaveChangesAsync();
+
+            return CreatedAtRoute("DefaultApi", new { id = game.Id }, game);
+        }
+
+        // PUT: api/games/5
+        [HttpPut]
+        [Route("{id:int}")]
+        public async Task<IHttpActionResult> UpdateGame(int id, [FromBody] Game game)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != game.Id)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(game).State = EntityState.Modified;
+
+            try
+            {
+                await db.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!GameExists(id))
+                {
+                    return NotFound();
+                }
+                throw;
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // DELETE: api/games/5
+        [HttpDelete]
+        [Route("{id:int}")]
+        public async Task<IHttpActionResult> DeleteGame(int id)
+        {
+            var game = await db.Game.FindAsync(id);
+            if (game == null)
+            {
+                return NotFound();
+            }
+
+            db.Game.Remove(game);
+            await db.SaveChangesAsync();
+
+            return Ok(game);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool GameExists(int id)
+        {
+            return db.Game.Count(e => e.Id == id) > 0;
+        }
+    }
+}
